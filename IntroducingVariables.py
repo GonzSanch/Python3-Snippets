30c1c112600b57fb41211cb37138d9dea870e4c3
Gonzalo <gonzalo.sanchezsilva@gmail.com>
2019-07-12 02:14:48 +0200

    Deleting IDE items adding py files

---
+++ IntroducingVariables.py
@@ -0,0 +1,13 @@
+#define variables
+character_name = "John"
+character_age = "35"
+is_male = True
+
+#introduce in print output
+print("There once was a man named "+ character_name +", ")
+print("he was "+character_age+" yeas old. ")
+
+#Reasign new value to variable
+character_name = "Mike"
+print("He really liked the name "+ character_name +", ")
+print("but didn't like being "+character_age+".")
---
+++ StringFunctions.py
@@ -0,0 +1,16 @@
+print("Shooting\nStars")
+print("Shooting\"Stars\n")
+
+phrase = "Some random text"
+print(phrase)
+print(phrase + " is cool")                      #Concatenation
+print(phrase.lower())                           #Lower cases
+print(phrase.upper())                           #Upper cases
+print(phrase.isupper())                         #boolean true if hole string is upper
+print(phrase.upper().isupper())                 
+print(len(phrase))                              #Give length of string
+print(phrase[0])                                #indexed 0 of string
+print(phrase.index("S"))                        #Find indexes of any snippet of string
+print(phrase.index("e"))
+print(phrase.index("text"))
+print(phrase.replace("random", "organized"))    #Replace some string value
